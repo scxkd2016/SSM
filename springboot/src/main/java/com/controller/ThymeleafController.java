c7edde52c388412c2987cf9bba9210d210af7c09
scxkd2016 <scxkd2015@gmail.com>
2018-11-21 17:18:06 +0800

    update

---
+++ springboot/src/main/java/com/controller/ThymeleafController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class ThymeleafController {
+    @RequestMapping("/show")
+    public String show(Model model) {
+        model.addAttribute("msg", "My first Thymeleaf");
+        model.addAttribute("key", new Date());
+        return "index";
+    }
+    @RequestMapping("/{page}")
+    public String show2(@PathVariable  String page) {
+        return page;
+    }
+    @RequestMapping("/show2")
+    public String show3() {
+        return "th_url";
+    }
+}
